daae97e84239ce57775b9325b9103d0463ade7cc
Sadnam Sakib <sadnamsakib@iut-dhaka.edu>
2023-03-01 16:02:01 +0600

    Adding boxicons

--- code/client/src/components/profileFormCustomer.js
+++ code/client/src/components/profileFormCustomer.js
@@ -8,43 +8,36 @@ function profileFormCustomer() {
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Pharmacy Name</Form.Label>
-        <Form.Control type="pharmacyName" placeholder="Enter name of your pharmacy" />
+        <Form.Control type="pharmacyName" placeholder="Enter name of your pharmacy" disabled/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Name of owner</Form.Label>
-        <Form.Control type="ownerName" placeholder="Enter name of the owner" />
+        <Form.Control type="ownerName" placeholder="Enter name of the owner" disabled />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Contact No.</Form.Label>
-        <Form.Control type="email" placeholder="Enter contact no." />
+        <Form.Control type="email" placeholder="Enter contact no." disabled />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Address</Form.Label>
-        <Form.Control type="address" placeholder="Address" />
+        <Form.Control type="address" placeholder="Address" disabled />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+        <Form.Control type="email" placeholder="Enter email"  disabled/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" disabled/>
         
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
+      
+      
     </Form>
   );
 }
