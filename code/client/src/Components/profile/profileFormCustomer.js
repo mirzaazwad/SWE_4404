dd90c0a570e3be0bd30624102109d7f18af09cc1
Sadnam Sakib <sadnamsakib@iut-dhaka.edu>
2023-03-08 11:26:45 +0600

    adding components of profile.

---
+++ code/client/src/Components/profile/profileFormCustomer.js
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import "../index.css";
+import "../boxicons-2.1.4/css/boxicons.min.css";
+
+function ProfileFormCustomer() {
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
+
+
+  const turnOnEdit = () => {
+    setIsDisabled(false);
+    setIsEditing(true);
+  }
+  const turnOffEdit = () => {
+    setIsDisabled(true);
+    setIsEditing(false);
+  }
+
+  return (
+    <div>
+      <div className="profileInfo d-flex justify-content-between">
+        <h4 className="InfoHeader mb-4">Personal Information</h4>
+        <button className="btn btn-outline-dark btn-editProfile " onClick={turnOnEdit}>Edit Profile
+        <i class='bx bx-cog bx-sm' ></i></button>
+      </div>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="Name" placeholder="Enter your name" disabled={isDisabled}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Contact No.</Form.Label>
+          <Form.Control type="email" placeholder="Enter contact no." disabled={isDisabled} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Address</Form.Label>
+          <Form.Control type="address" placeholder="Address" disabled={isDisabled}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email"  disabled={isDisabled}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" disabled={isDisabled}/>
+        </Form.Group>
+      
+        {isEditing && (
+          <Button className="btn btn-outline-dark btn-save" type="submit" onClick={turnOffEdit}>
+            Save
+          </Button>
+        )}</Form>
+    </div>
+  );
+}
+
+export default ProfileFormCustomer;
