14062b7a22c1f18222d4575abb8e43fd986451c5
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2023-03-11 20:23:37 +0600

    forgot Password

--- code/server/controller/profile/forgotPassword/forgotPasswordController.js
+++ code/server/controller/profile/forgotPassword/forgotPasswordController.js
@@ -3,9 +3,9 @@ const sellerModel = require("../../../model/seller/sellerModel");
 
 
 const userExists = async (req, res) => {
-  const { email} = req.body;
-  const searchBuyer = await buyerModel.find({ email: email });
-  const searcSeller = await sellerModel.find({ email: email });
+  const email = req.body.email;
+  const searchBuyer = await buyerModel.findOne({ email: email });
+  const searcSeller = await sellerModel.findOne({ email: email });
   if (!searchBuyer && !searcSeller) {
     return res.status(404).json({ error: "user account does not exist" });
   }
