78bf9ec9b17b1809eeefe5e964f3156120fab840
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-03-11 23:06:51 +0600

    Restructuring and removing redundancy

--- code/server/routes/profile/changePassword/changePasswordRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const {patchPasswordByID, findUser} = require('../../../controller/profile/changePassword/changePasswordController');
-const router = express.Router();
-router.use(express.json());
-router.post('/:id', findUser);
-router.patch('/:id', patchPasswordByID);
-module.exports = router;
