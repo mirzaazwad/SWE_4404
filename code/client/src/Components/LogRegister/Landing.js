dbc2d738c71dc43e2b4f06dbb4769bc23701fde5
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-03-12 09:47:36 +0600

    fixing merge issues

--- code/client/src/Components/LogRegister/Landing.js
+++ code/client/src/Components/LogRegister/Landing.js
@@ -1,18 +1,12 @@
 import LandingImage from "../partials/landing/image";
-import Introduction from "../partials/landing/introduction";
 import NavbarLanding from "../partials/landing/navbarLanding";
-import { Provider} from "react-redux";
-import { store } from "../../Contexts/store";
 import LoginSignUp from "./LoginSignUp";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from "./Login";
 
 const Landing = () => {
   return (
     <div className="Landing">
     <div><NavbarLanding /></div>
-        
-        <Provider store={store}>
           <section className="h-100">
             <div className="container fluid d-flex justify-content-around">
             <div className="landing-image-container my-auto">
@@ -21,8 +15,6 @@ const Landing = () => {
               <div className="login"><LoginSignUp/></div>
             </div>
           </section>
-          </Provider>
-
     </div>
   );
 };
