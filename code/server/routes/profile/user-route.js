9168eb384ea55ca77bcf5e55a89fd59f137f6e8f
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-03-13 00:42:08 +0600

    Change Password Profile Fully Functional

--- code/server/routes/profile/user-route.js
+++ code/server/routes/profile/user-route.js
@@ -1,12 +1,10 @@
 const express=require('express');
-const {getUserByID, patchUserByID,changePassword, verifyPassword} = require('../../controller/profile-controller');
+const {getUserByID,changePassword} = require('../../controller/profile-controller');
 const requireAuth = require('../../middleware/requireAuth');
 const router=express.Router();
 
 router.use(requireAuth);
-router.post('/verify',verifyPassword);
-router.get('/:id',getUserByID);
-router.patch('/:id',patchUserByID);
-router.post('/',changePassword);
+router.get('/getUser/:id',getUserByID);
+router.patch('/changePassword/:id',changePassword); 
 
 module.exports = router;
