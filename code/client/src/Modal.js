d5fa82ccc8b91c78617666b2ba8f908369e90cbc
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2023-04-28 04:06:07 +0600

    modal for my cart is working perfectly

--- code/client/src/Modal.js
+++ code/client/src/Modal.js
@@ -3,14 +3,17 @@ import ReactDom from 'react-dom'
 
 const MODAL_STYLES = {
   position: 'fixed',
-  top: '60%',
+  top: '55%',
   left: '50%',
   backgroundColor: '#fff',
   transform: 'translate(-50%, -50%)',
   zIndex: 1000,
-  height: '90%',
-  width: '90%'
-}
+  height: '80%',
+  width: '80%',
+  padding: '2rem',
+  borderRadius: '0.5rem',
+  boxShadow: '0 5px 16px rgba(0, 0, 0, 0.2)'
+};
 
 const OVERLAY_STYLES = {
   position: 'fixed',
@@ -18,9 +21,9 @@ const OVERLAY_STYLES = {
   left: 0,
   right: 0,
   bottom: 0,
-  backgroundColor: 'rgba(0, 0, 0, .7)',
+  backgroundColor: 'rgba(0, 0, 0, 0.7)',
   zIndex: 1000
-}
+};
 
 export default function Modal({ children, onClose }) {
 
@@ -28,7 +31,7 @@ export default function Modal({ children, onClose }) {
     <>
       <div style={OVERLAY_STYLES} />
       <div style={MODAL_STYLES}>
-        <button className='btn bg-danger fs-4' style={{ marginLeft: "90%", marginTop: "-35px" }} onClick={onClose}> X </button>
+        <button className='btn bg-danger fs-4' style={{ marginLeft: "100%", marginTop: "-6%" }} onClick={onClose}> X </button>
         {children}
       </div>
     </>,
