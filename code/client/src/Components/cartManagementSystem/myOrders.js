a9974de961f982ea5404047531f7074412f249cd
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2023-04-28 04:09:20 +0600

    my orders page creation

---
+++ code/client/src/Components/cartManagementSystem/myOrders.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import OrderCard from "./orderCard";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import NavbarCustomer from "../partials/profile/navbarCustomer";
+
+const MyOrders = () => {
+  const [orders, setOrders] = useState([]);
+  const user = localStorage.getItem("user");
+  const userId = JSON.parse(user)._id;
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const res = await axios.get(`http://localhost:4000/api/order/getOrder/${userId}`);
+        setOrders(res.data.order_data);
+        console.log("orders");
+        console.log(res.data.order_data);
+      } catch (err) {
+        if (err.response && err.response.status === 401) {
+            console.log("Failed to fetch order");
+        } else {
+          console.error(err);
+        }
+      }
+    };
+
+    fetchOrders();
+  }, []);
+
+  return (
+    <div>
+      <div className="mb-5">
+        <NavbarCustomer />
+      </div>
+      <div className='my-orders d-flex justify-content-center'>
+          <div className="d-flex flex-column w-50">
+          {orders.map((order) => (
+              <OrderCard key={order.index} order={order} />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyOrders;
