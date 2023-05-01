3c200a5ccad006d3e60848db67b740e2e1032abb
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-05-01 14:24:07 +0600

    Fixing address display issue  google map in use

--- code/client/src/Components/viewPharmacies/pharmacyCard.js
+++ code/client/src/Components/viewPharmacies/pharmacyCard.js
@@ -2,16 +2,38 @@ import React from 'react';
 import { useEffect, useState } from "react";
 import { Card } from 'react-bootstrap';
 
-const PharmacyCard = ({ name, location, image }) => {
-  const [isHovered, setIsHovered] = useState(false);
+const PharmacyCard = (props) => {
+
+  const [address,setAddress]=useState("");
+  
+  const getPlaceDetails = async (lat, lng) => {
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.REACT_APP_GMPKEY}`
+    );
+    const data = await response.json();
+    if (data.status === "OK") {
+      setAddress(data.results[0].formatted_address);
+      return data.results[0].formatted_address;
+    } else {
+      console.log("Geocode was not successful for the following reason:", data.status);
+      return null;
+    }
+  };
+
+  useEffect(()=>{
+    if(props.location){
+      getPlaceDetails(props.location.lat,props.location.lng);
+    }
+  },[props])
+
   return (
     <Card
       className="pharmacy-card"
     >
-      <Card.Img variant="top" src={image} className="pharmacy-card-image" />
+      <Card.Img variant="top" src={props.image} className="pharmacy-card-image" />
       <Card.Body >
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>{location}</Card.Text>
+        <Card.Title>{props.name}</Card.Title>
+        <Card.Text>{address}</Card.Text>
       </Card.Body>
     </Card>
   );
